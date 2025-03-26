f1a6b23841809ad6c7fd5f2223d221c2a930c9b4
irfanshafee <irfanshafee1@gmail.com>
2025-03-26 23:34:09 +0600

    refactor: extract table printing logic into separate methods
    Extract repetitive table header, border, and formatting logic into dedicated methods to improve code maintainability and readability. This reduces code duplication and makes the display logic more modular.

--- src/FlightReservation.java
+++ src/FlightReservation.java
@@ -151,46 +151,77 @@ public class FlightReservation implements DisplayClass {
         }
     }
 
-    /*toString() Method for displaying number of flights registered by single user...*/
-    public String toString(int serialNum, Flight flights, Customer customer) {
-        return String.format("| %-5d| %-41s | %-9s | \t%-9d | %-21s | %-22s | %-10s  |   %-6sHrs |  %-4s  | %-10s |", serialNum, flights.getFlightSchedule(), flights.getFlightNumber(), customer.numOfTicketsBookedByUser.get(serialNum - 1), flights.getFromWhichCity(), flights.getToWhichCity(), flights.fetchArrivalTime(), flights.getFlightTime(), flights.getGate(), flightStatus(flights));
-    }
-
-    @Override
-    public void displayFlightsRegisteredByOneUser(String userID) {
+    private void printFlightTableHeader() {
         System.out.println();
         System.out.print("+------+-------------------------------------------+-----------+------------------+-----------------------+------------------------+---------------------------+-------------+--------+-----------------+\n");
         System.out.printf("| Num  | FLIGHT SCHEDULE\t\t\t   | FLIGHT NO |  Booked Tickets  | \tFROM ====>>       | \t====>> TO\t   | \t    ARRIVAL TIME       | FLIGHT TIME |  GATE  |  FLIGHT STATUS  |%n");
         System.out.print("+------+-------------------------------------------+-----------+------------------+-----------------------+------------------------+---------------------------+-------------+--------+-----------------+\n");
+    }
+
+    private void printFlightTableBorder() {
+        System.out.print("+------+-------------------------------------------+-----------+------------------+-----------------------+------------------------+---------------------------+-------------+--------+-----------------+\n");
+    }
+
+    private String formatFlightInfo(int serialNum, Flight flights, Customer customer) {
+        return String.format("| %-5d| %-41s | %-9s | \t%-9d | %-21s | %-22s | %-10s  |   %-6sHrs |  %-4s  | %-10s |", 
+            serialNum, 
+            flights.getFlightSchedule(), 
+            flights.getFlightNumber(), 
+            customer.numOfTicketsBookedByUser.get(serialNum - 1), 
+            flights.getFromWhichCity(), 
+            flights.getToWhichCity(), 
+            flights.fetchArrivalTime(), 
+            flights.getFlightTime(), 
+            flights.getGate(), 
+            flightStatus(flights));
+    }
+
+    @Override
+    public void displayFlightsRegisteredByOneUser(String userID) {
+        printFlightTableHeader();
         for (Customer customer : Customer.customerCollection) {
             List<Flight> f = customer.getFlightsRegisteredByUser();
             int size = customer.getFlightsRegisteredByUser().size();
             if (userID.equals(customer.getUserID())) {
                 for (int i = 0; i < size; i++) {
-                    System.out.println(toString((i + 1), f.get(i), customer));
-                    System.out.print("+------+-------------------------------------------+-----------+------------------+-----------------------+------------------------+---------------------------+-------------+--------+-----------------+\n");
+                    System.out.println(formatFlightInfo((i + 1), f.get(i), customer));
+                    printFlightTableBorder();
                 }
             }
         }
     }
 
-    /*overloaded toString() method for displaying all users in a flight....*/
-
-    public String toString(int serialNum, Customer customer, int index) {
-        return String.format("%10s| %-10d | %-10s | %-32s | %-7s | %-27s | %-35s | %-23s |       %-7s  |", "", (serialNum + 1), customer.randomIDDisplay(customer.getUserID()), customer.getName(),
-                customer.getAge(), customer.getEmail(), customer.getAddress(), customer.getPhone(), customer.numOfTicketsBookedByUser.get(index));
-    }
-
-    @Override
-    public void displayHeaderForUsers(Flight flight, List<Customer> c) {
+    private void printCustomerTableHeader(Flight flight) {
         System.out.printf("\n%65s Displaying Registered Customers for Flight No. \"%-6s\" %s \n\n", "+++++++++++++", flight.getFlightNumber(), "+++++++++++++");
         System.out.printf("%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+----------------+\n", "");
         System.out.printf("%10s| SerialNum  |   UserID   | Passenger Names                  | Age     | EmailID\t\t       | Home Address\t\t\t     | Phone Number\t       | Booked Tickets |%n", "");
         System.out.printf("%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+----------------+\n", "");
+    }
+
+    private void printCustomerTableBorder() {
+        System.out.printf("%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+----------------+\n", "");
+    }
+
+    private String formatCustomerInfo(int serialNum, Customer customer, int index) {
+        return String.format("%10s| %-10d | %-10s | %-32s | %-7s | %-27s | %-35s | %-23s |       %-7s  |", 
+            "", 
+            (serialNum + 1), 
+            customer.randomIDDisplay(customer.getUserID()), 
+            customer.getName(),
+            customer.getAge(), 
+            customer.getEmail(), 
+            customer.getAddress(), 
+            customer.getPhone(), 
+            customer.numOfTicketsBookedByUser.get(index));
+    }
+
+    @Override
+    public void displayHeaderForUsers(Flight flight, List<Customer> c) {
+        printCustomerTableHeader(flight);
         int size = flight.getListOfRegisteredCustomersInAFlight().size();
         for (int i = 0; i < size; i++) {
-            System.out.println(toString(i, c.get(i), flightIndex(c.get(i).flightsRegisteredByUser, flight)));
-            System.out.printf("%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+----------------+\n", "");
+            System.out.println(formatCustomerInfo(i, c.get(i), flightIndex(c.get(i).flightsRegisteredByUser, flight)));
+            printCustomerTableBorder();
         }
     }
 
